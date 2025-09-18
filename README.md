6e1272130a15e582c484fed6ed0b77262d4d5d35
jeffyactive <jeff@reelyactive.com>
2025-09-18 12:14:47 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# reposition
-Convert positions from one coordinate system to another.  We believe in an open Internet of Things.
+reposition
+==========
+
+Convert positions from one coordinate system to another.
+
+
+Hello reposition!
+-----------------
+
+```javascript
+const reposition = require('reposition');
+
+const COORDINATE_SYSTEM = {};
+
+let position = [ -123, 456, 0 ];
+let convertedPosition = reposition.toWGS84(position, COORDINATE_SYSTEM);
+
+console.log(convertedPosition);
+```
+
+Which should yield `[ 1, 2, 3]`.
+
+
+Coordinate Systems
+------------------
+
+### customOrigin
+
+A __customOrigin__ coordinate system uses an origin _other than_ 0° longitude, 0° latitude and measurements in standard units of a given type in each axis.
+
+    {
+      originOffset: [ -73.57123, 45.50883, 50 ],
+      measurementUnits: [ 'deg', 'deg', 'm' ],
+      horizontalPlaneRotation: 0
+    }
+
+At present, the origin offset coordinate system is assumed to be WGS84.  In future, an additional property may be added to specify this coordinate system.
+
+
+Contributing
+------------
+
+Discover [how to contribute](CONTRIBUTING.md) to this open source project which upholds a standard [code of conduct](CODE_OF_CONDUCT.md).
+
+
+Security
+--------
+
+Consult our [security policy](SECURITY.md) for best practices using this open source software and to report vulnerabilities.
+
+
+License
+-------
+
+MIT License
+
+Copyright (c) 2025 [reelyActive](https://www.reelyactive.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+THE SOFTWARE.
---
+++ test/unit/reposition.test.js
@@ -0,0 +1,73 @@
+/**
+ * Copyright reelyActive 2025
+ * We believe in an open Internet of Things
+ */
+
+
+const reposition = require('../../lib/reposition.js');
+const assert = require ('assert');
+
+
+// Input data for the scenario
+const INPUT_DATA_INVALID_POSITION = { lat: 123, lon: 456 };
+const INPUT_DATA_INVALID_SYSTEM = {};
+const INPUT_DATA_CUSTOM_ORIGIN_POSITION_DEG = [ -0.00877, 0.01117, 10 ];
+const INPUT_DATA_CUSTOM_ORIGIN_POSITION_FT = [ -2240.450648739, 4071.88290589, 32.8084 ];
+const INPUT_DATA_CUSTOM_ORIGIN_SYSTEM = {
+    type: "customOrigin",
+    originOffset: [ -73.57123, 45.50883, 0 ]
+};
+const INPUT_DATA_CUSTOM_ORIGIN_SYSTEM_FT = {
+    type: "customOrigin",
+    originOffset: [ -73.57123, 45.50883, 0 ],
+    measurementUnits: [ 'ft', 'ft', 'ft' ]
+};
+const INPUT_DATA_CUSTOM_ORIGIN_SYSTEM_ROTATED = {
+    type: "customOrigin",
+    originOffset: [ -73.57123, 45.50883 ],
+    horizontalPlaneRotation: 45
+};
+
+
+// Expected outputs for the scenario
+const EXPECTED_DATA_INVALID_INPUT = null;
+const EXPECTED_DATA_CUSTOM_ORIGIN = [ -73.58, 45.52, 10 ];
+
+
+// Describe the scenario
+describe('reposition', () => {
+
+  // Test the toWGS84 function with no input data
+  it('should handle no input data', () => {
+    assert.deepEqual(reposition.toWGS84(), EXPECTED_DATA_INVALID_INPUT);
+  });
+
+  // Test the toWGS84 function with an invalid position
+  it('should handle an invalid position', () => {
+    assert.deepEqual(reposition.toWGS84(INPUT_DATA_INVALID_POSITION,
+                                        INPUT_DATA_CUSTOM_ORIGIN_SYSTEM),
+                     EXPECTED_DATA_INVALID_INPUT);
+  });
+
+  // Test the toWGS84 function with an invalid coordinate system
+  it('should handle an invalid coordinate system', () => {
+    assert.deepEqual(reposition.toWGS84(INPUT_DATA_CUSTOM_ORIGIN_POSITION_DEG,
+                                        INPUT_DATA_INVALID_SYSTEM),
+                     EXPECTED_DATA_INVALID_INPUT);
+  });
+
+  // Test the toWGS84 function with a custom origin
+  it('should handle a customOrigin coordinate system', () => {
+    assert.deepEqual(reposition.toWGS84(INPUT_DATA_CUSTOM_ORIGIN_POSITION_DEG,
+                                        INPUT_DATA_CUSTOM_ORIGIN_SYSTEM),
+                     EXPECTED_DATA_CUSTOM_ORIGIN);
+  });
+
+  // Test the toWGS84 function with a custom origin using feet
+  it('should handle a customOrigin coordinate system using feet', () => {
+    assert.deepEqual(reposition.toWGS84(INPUT_DATA_CUSTOM_ORIGIN_POSITION_FT,
+                                        INPUT_DATA_CUSTOM_ORIGIN_SYSTEM_FT),
+                     EXPECTED_DATA_CUSTOM_ORIGIN);
+  });
+
+});
